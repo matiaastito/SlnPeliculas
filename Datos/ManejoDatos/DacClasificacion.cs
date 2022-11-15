f110dd461d77eca1c976738cd7e5309ed1d07f15
matiaastito <81437963+matiaastito@users.noreply.github.com>
2022-11-15 13:12:53 -0300

    Update DacClasificacion.cs

--- Datos/ManejoDatos/DacClasificacion.cs
+++ Datos/ManejoDatos/DacClasificacion.cs
@@ -1,12 +1,35 @@
-﻿using System;
+using Datos.DBDatos;
+using LibPeliculas;
+using LibPeliculas.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Datos.Datos
+namespace Datos.ManejoDatos
 {
-    internal class DacClasificacion
+    public class DacClasificacion
     {
+        private static DBPeliculasContext context = new DBPeliculasContext();
+
+        public static List<Clasificacion> Listar()
+        {
+            return context.Clasificaciones.ToList();
+        }
+
+        public static Clasificacion TraerUno(string tClasificacion)
+        {
+            List<Clasificacion> listClasificaciones = Listar();
+
+            foreach (var clasificacion in listClasificaciones)
+            {
+                if (string.Compare(clasificacion.Tipo.ToLower(), tClasificacion.ToLower()) == 0)
+                {
+                    return clasificacion;
+                }
+            }
+            return null;
+        }
     }
 }
